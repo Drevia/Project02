8cc76f5bd238a129d02afb7ddcfec6ed51084560
Drevia <drevia59@gmail.com>
2022-11-24 09:53:53 +0100

    adding class for writting symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsWriter.java
@@ -1,4 +1,22 @@
 package com.hemebiotech.analytics;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Permet d'écrire dans un noçuveau fichier les symptoms obtenus
+ * grace à AnalyticsCounter et de noter leur nombres d'occurences
+ */
 public class AnalyticsWriter {
+    
+    public static void writeSymptoms(TreeMap<String, Integer> sorted) throws IOException {
+        FileWriter writer = new FileWriter("results.out");
+
+        for (Map.Entry<String, Integer> entry: sorted.entrySet()) {
+            writer.write(entry.getKey() + " = " + entry.getValue() + "\n");
+        }
+        writer.close();
+    }
 }
